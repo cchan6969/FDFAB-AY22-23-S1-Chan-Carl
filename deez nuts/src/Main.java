018162d99cb3461fd2832fd82e9f41c89ea04296
cchan6969 <cchan.20@ichat.sp.edu.sg>
2022-06-16 10:08:55 +0800

    Add in pcb.html

--- deez nuts/src/Main.java
+++ deez nuts/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
   public static void main(String[] args) 
   {
-    String name ="511";
-    System.out.println(name);
+    int num = 500;
+    String result =(num >= 500)?"Yes! ":"No! ";
+    System.out.println(result +"It is more than or equal to 500");
   }
 }
